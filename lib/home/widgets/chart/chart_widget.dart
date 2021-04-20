45d5ab05a9fde1063b9e7d8a807ab3aad0a1b869
lucasew <lucas59356@gmail.com>
2021-04-20 13:46:46 -0300

    mexi no layout do gráfico, no desktop tava bugado
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- lib/home/widgets/chart/chart_widget.dart
+++ lib/home/widgets/chart/chart_widget.dart
@@ -6,21 +6,23 @@ class ChartWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        height: 80,
-        width: 80,
-        child: Stack(children: [
-          Center(
-              child: Container(
-                  height: 80,
-                  width: 80,
-                  child: CircularProgressIndicator(
-                      strokeWidth: 10,
-                      value: .75,
-                      backgroundColor: AppColors.chartSecondary,
-                      valueColor: AlwaysStoppedAnimation<Color>(
-                          AppColors.chartPrimary)))),
-          Center(child: Text("75%", style: AppTextStyles.heading))
-        ]));
+    return ConstrainedBox(
+        constraints: BoxConstraints(maxHeight: 80, maxWidth: 80),
+        child: FittedBox(
+            child: Padding(
+                padding: EdgeInsets.only(left: 12),
+                child: Stack(
+                    fit: StackFit.loose,
+                    alignment: Alignment.center,
+                    children: [
+                      Center(
+                          child: CircularProgressIndicator(
+                              strokeWidth: 10,
+                              value: .75,
+                              backgroundColor: AppColors.chartSecondary,
+                              valueColor: AlwaysStoppedAnimation<Color>(
+                                  AppColors.chartPrimary))),
+                      Center(child: Text("75%", style: AppTextStyles.heading.merge(TextStyle(fontSize: 10))))
+                    ]))));
   }
 }
