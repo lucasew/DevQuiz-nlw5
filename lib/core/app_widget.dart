410117cd0b578d4e0a50203f1fe98730530bd636
lucasew <lucas59356@gmail.com>
2021-04-19 21:06:32 -0300

    lib aula
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -1,13 +1,14 @@
-// import 'package:DevQuiz/home/home_page.dart';
+import 'package:DevQuiz/home/home_page.dart';
 import 'package:DevQuiz/splash/splash_page.dart';
 import 'package:flutter/material.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    print("widget");
     return MaterialApp(
       title: "DevQuiz",
-      home: SplashPage(),
+      home: HomePage(),
     );
   }
 }
