ed0012204cd6a85ae865b84c77502f0838d5f5d4
lucasew <lucas59356@gmail.com>
2021-04-22 08:31:46 -0300

    consegui fazer meu enum diferenciado com level_button
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- lib/home/widgets/level_button/level_button_widget.dart
+++ lib/home/widgets/level_button/level_button_widget.dart
@@ -4,20 +4,66 @@ import '../../../core/core.dart';
 
 class LevelButtonWidget extends StatelessWidget {
   final String label;
-  const LevelButtonWidget({Key? key, required this.label}) : super(key: key);
+  final Color borderColor;
+  final Color buttonColor;
+  final Color textColor;
+
+  LevelButtonWidget._(
+      {required this.label,
+      required this.borderColor,
+      required this.textColor,
+      required this.buttonColor,
+      Key? key})
+      : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Container(
         decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(28),
-            color: AppColors.levelButtonFacil,
-            border: Border.fromBorderSide(
-                BorderSide(color: AppColors.levelButtonBorderFacil))),
+            color: this.buttonColor,
+            border: Border.fromBorderSide(BorderSide(color: this.borderColor))),
         child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 26, vertical: 6),
             child: Text(label,
                 style: GoogleFonts.notoSans(
-                    color: AppColors.levelButtonFacil, fontSize: 13))));
-// FIXME: cadê o texto :v
+                    color: this.textColor, fontSize: 13))));
+    // FIXME: cadê o texto :v
+  }
+
+  static LevelButtonWidget facil({Key? key}) {
+    return LevelButtonWidget._(
+        borderColor: AppColors.levelButtonBorderFacil,
+        textColor: AppColors.levelButtonTextFacil,
+        buttonColor: AppColors.levelButtonFacil,
+        label: "Fácil",
+        key: key);
+  }
+
+  static LevelButtonWidget medio({Key? key}) {
+    return LevelButtonWidget._(
+        borderColor: AppColors.levelButtonBorderMedio,
+        textColor: AppColors.levelButtonTextMedio,
+        buttonColor: AppColors.levelButtonMedio,
+        label: "Médio",
+        key: key);
+  }
+
+  static LevelButtonWidget dificil({Key? key}) {
+    return LevelButtonWidget._(
+        borderColor: AppColors.levelButtonBorderDificil,
+        textColor: AppColors.levelButtonTextDificil,
+        buttonColor: AppColors.levelButtonDificil,
+        label: "Difícil",
+        key: key);
+  }
+
+  static LevelButtonWidget perito({Key? key}) {
+    return LevelButtonWidget._(
+        borderColor: AppColors.levelButtonBorderPerito,
+        textColor: AppColors.levelButtonTextPerito,
+        buttonColor: AppColors.levelButtonPerito,
+        label: "Perito",
+        key: key);
   }
 }
